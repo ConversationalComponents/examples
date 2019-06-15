cd8216111338c87e77c08ac9b199e0f7cc078973
mor91 <mor@buskilla.com>
2019-06-15 19:57:03 +0300

    add console tester and coffeeshop example

---
+++ coffeeshop/actions.py
@@ -0,0 +1,6 @@
+
+def get_drink_action(first_name=""):
+    if first_name:
+        return f"Nice to meet you {first_name}, What can I get you to drink?"
+    return "Hi, What can I get you to drink?"
+
---
+++ coffeeshop/components_coco.py
@@ -0,0 +1,11 @@
+from .coco import call_coco
+from .actions import get_drink_action
+
+def namer_comp(state, inp):
+    response = call_coco("namer_vp3", state["session_id"], user_input=inp)
+    if response["component_done"]:
+        state["name"] = response["updated_context"]
+        first_name = response["updated_context"].get("userfirstName")
+        return [get_drink_action(first_name)], None, True
+    return [response["response"]], None, False
+
